965fb593c00380240e4c8af854ad7c030348a3d3
alex baehr <alnbahr@gmail.com>
2020-11-23 09:24:27 -0800

    contrast operstion, documentation

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # abMaya
-Collection of tools for the Autodesk Maya DCC
+Collection of tools for the Autodesk Maya DCC package.
+
+## contextManagers
+Context Managers used to safely make temptorary changes to an open Maya scene.
+
+## ngSkinTools_extensions
+Additional tools, brushes and functions for the ngSkinTools plugin used for skin weight painting in the rigging department.  
---
+++ ngSkinTools_extensions/python/core/brushes.py
@@ -0,0 +1,16 @@
+
+# ----------------------------------------------------------------------------------------------- #
+# IMPORTS
+from math import pow, sqrt
+from scipy.stats import norm
+# ----------------------------------------------------------------------------------------------- #
+
+
+# ----------------------------------------------------------------------------------------------- #
+# True constrast operation that preserves minimum and maximum values. 
+def contrast(value, weight, vtx_min, vtx_max):
+    norm_range = vtx_max - vtx_min                                               # find normal range
+    difference = weight - ((vtx_max + vtx_min) / 2)                              # vtx_weight - min/max average 
+    multiplier = norm.cdf(difference, loc=0, scale=(0.1 * norm_range)) - weight  # apply normal random variable    
+    return max(vtx_min, min(weight + (value * multiplier) , vtx_max))            # clamp constrast to 
+# ----------------------------------------------------------------------------------------------- #
